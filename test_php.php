a28ef358c2a08aed9596479cfd64759941f0b9f1
Ho0108 <111258752+Ho0108@users.noreply.github.com>
2022-08-17 14:45:20 +0800

    Create test_php.php

---
+++ test_php.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+  
+<?php 
+ 
+  $test = '123' ;
+  
+ ?>  
+  
+
+<h1>My First Heading</h1>
+<h2><?php echo $test ; ?></h2>
+<p>My first paragraph.</p>
+
+</body>
+</html>
